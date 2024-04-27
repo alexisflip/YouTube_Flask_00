d977618feec8752b219a75711e8b26d5244d95e4
alexisflip <161642061+alexisflip@users.noreply.github.com>
2024-04-27 21:39:51 +0200

    - render template - render template with arguments - basic html file with functions

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-print ('Hello World')
-#Test Git commint
-#Second commit
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+@app.route('/<name>')
+def user(name):
+    return f'Hello {name}'
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('user', name='Admin!'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
