046226b1de2ff10b4e8a565b041f05c086f20415
alexisflip <161642061+alexisflip@users.noreply.github.com>
2024-04-27 22:29:28 +0200

    - bootstrap: navbar - template inheritance

--- 00_tutorial.py
+++ 00_tutorial.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', content='yo')
 
 @app.route('/<name>')
 def user(name):
     return render_template('index.html', content=name)
+
+@app.route('/newpage')
+def newpage():
+    return render_template('newpage.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
